d711b818e97997fccd8f779066ce91bec70b9401
Shibilkhan999 <159814940+Shibilkhan999@users.noreply.github.com>
2024-08-22 12:53:30 -0700

    Add files via upload

---
+++ thrusday/alert/script.js
@@ -0,0 +1,11 @@
+// var stuname =("first class in javascript \n second class in javascript")
+// alert(stuname);
+
+
+alert("Hello! I am an alert box!");
+
+alert("Welcome to js land \n happy coding");
+
+alert("Happy coding!");
+
+console.log("Hello I can run js through my web browser");
---
+++ thrusday/variable/script.js
@@ -0,0 +1,34 @@
+var username;
+
+var myName = "Shibil khan";
+alert (myName);
+
+var message = "Hello World";
+alert(message);
+
+
+var studentName = "John Doe"; 
+alert("Name: " + studentName);
+var studentAge = 20; 
+alert("Age: " + studentAge);
+var studentGrade = "A"; 
+alert("Grade: " + studentGrade);
+
+var studentSchool = "XYZ High School"; 
+alert("School: " + studentSchool);
+
+var stuname =("first class in javascript \n second class in javascript");
+alert(stuname);
+
+var used = ("Email Address(e.g. example@example.com)");
+alert (used);
+
+var book = "A smarter way to learn JavaScript";
+alert (book);
+
+
+var end = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+alert (end);
+
+
+
